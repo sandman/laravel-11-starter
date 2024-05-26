2d142221d4d4959c45584ff41c97f34ef515b7dc
Sandip Gangakhedkar <sandip.gangakhedkar@gmail.com>
2024-05-27 00:45:20 +0100

    Starter project for a Note taking app

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Note-taking app</title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased">
+        @session('message')
+            <div class="message">
+                {{ session('message') }}
+            </div>
+            
+        @endsession
+        {{ $slot }}
+    </body>
+</html>
---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,4 @@
+<x-app-layout>
+    <h1>403 Forbidden</h1>
+    <p>Sorry, you are not authorized to access this page.</p>
+</x-app-layout>
